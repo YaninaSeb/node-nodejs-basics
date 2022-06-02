4897bf1d602a7e84980b65acb0c5f0bc1f6466c5
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-06-02 17:02:19 +0300

    feat: add solution create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 export const create = async () => {
-    // Write your code here 
-};
+    const __dirname = path.resolve(path.dirname(""));
+    const filename = path.join(__dirname, 'files', 'fresh.txt');
+    const text = 'I am fresh and young';
+
+    try {
+        await fs.promises.writeFile(filename, text, { flag: 'wx'})
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
+}
+
+create();
