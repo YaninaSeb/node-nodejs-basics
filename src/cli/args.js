2001eab6c0f0e642f92abe6eb9ddafd5952cf177
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-06-03 13:14:22 +0300

    feat: add 'Command line interface' solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let res = [];
+    let allArgs = process.argv;
+    allArgs.forEach((arg, index) => {
+        if (/--/.test(arg)) {
+            res.push(`${allArgs[index].slice(2)} is ${allArgs[index+1]}`);
+        }
+    });
+    console.log(res.join(', '));
 };
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
-export const parseEnv = () => {
-    // Write your code here 
+ export const parseEnv = () => {
+    let res = [];
+    let allEnv = Object.keys(process.env);
+    allEnv.forEach((e) => {
+        if (/\bRSS_/.test(e)) {
+            res.unshift(`${e}=${process.env[e]}`);
+        }
+    });
+    console.log(res.join('; '));
 };
