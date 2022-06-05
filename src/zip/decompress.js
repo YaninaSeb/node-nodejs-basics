54fe67479baeaee01337ba0fe87d74a4faa52dee
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-06-05 16:52:33 +0300

    feat: add 'Zlib' solutions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,27 @@
+import path from 'path';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+
+const _filename = fileURLToPath(import.meta.url);
+const __dirname =  path.dirname(_filename);
+
 export const decompress = async () => {
-    // Write your code here 
+    const pathToZip = path.join(__dirname, 'files', 'archive.gz');
+    const pathToDecompressFile = path.join(__dirname, 'files', 'fileToCompress2.txt');
+
+    const unzip = zlib.createGunzip();
+    const source = createReadStream(pathToZip);
+    const destination = createWriteStream(pathToDecompressFile);
+
+    return pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.error('Error:', err);
+            process.exitCode = 1;
+        }
+    });
 };
+
+//test
+await decompress();
